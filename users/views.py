ae904853e055e58ffcfcd65a3899dc9603f32385
Men6ay <tamirlan2015@mail.ru>
2021-10-07 19:20:46 +0600

    'user'

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect
-from django.contrib import messages
-from users.models import Profile
+from django.shortcuts import render, redirect
+from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
@@ -17,29 +17,29 @@ def signup(request):
                 user = User.objects.create(username = username)
                 user.set_password(password1)
                 user.save()
-                profile = Profile.objects.create(user = user, nickname=nickname,image=image)
-                user = authenticate(username=username,password = password1)
-                login(request,user)
-                return redirect('index')
+                Profile.objects.create(user = user, nickname = nickname, image = image)
+                user = authenticate(username = username, password = password1)
+                login(request, user)
+                return redirect('data')
             except:
-                messages.error(request,'NOT CORRECT VALUE')
+                messages.error(request, 'Такой логин существует')
         else:
-            messages.error(request,'NOT CORRECT PASSWORD')
-    return render(request,'users/register.html')
+            messages.error(request, 'Не совподают пароли')
+    return render(request, 'users/register.html')
 
-def login(request):
+def login_user(request):
     if request.method == 'POST':
         try:
             username = request.POST.get('username')
             password = request.POST.get('password')
             user = User.objects.get(username=username)
-            user = authenticate(username=username,password=password)
-            login(request,user)
-            return redirect('index')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            redirect('data')
         except:
-            messages.error(request, 'NOT CORRECT LOGIN OR PASSWORD')
-    return render(request,'users/login.html')
+            messages.error(request, 'Не правильный логин или пароль')
+    return render(request, 'users/login.html')
 
-def profile(request,id):
-    profiles = Profile.objects.get(user__id =id)
-    return render(request, 'users/profile.html', {'profile':profiles})
+def profile(request, id):
+    profiles = User.objects.get(id=id)
+    return render(request, 'users/profile.html', {'profile' : profiles})
